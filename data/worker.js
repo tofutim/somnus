cfd6f78bc68dc58e813f176e7e2cc39d11f7320a
Tim Uy <tim@loqu8.com>
2016-03-21 16:16:42 -0700

    try to call but crashes firefox

--- data/worker.js
+++ data/worker.js
@@ -1,22 +1,33 @@
-self.onmessage = function(msg) {
-  self.postMessage('hello ' + msg.data);
-};
-
 self.onerror = function(msg) {
   self.postMessage('error: ' + msg);
 };
 
-var user32 = ctypes.open("user32.dll");
-var msgBox = user32.declare("MessageBoxW",
-                         ctypes.winapi_abi,
-                         ctypes.int32_t,
-                         ctypes.int32_t,
-                         ctypes.jschar.ptr,
-                         ctypes.jschar.ptr,
-                         ctypes.int32_t);
+self.onmessage = function(msg) {
+  self.postMessage('hello ' + msg.data);
+};
 
-
+self.postMessage('importing...');
+importScripts("resource://gre/modules/workers/require.js");
+importScripts("resource://somnus/data/ostypes/ostypes_win.jsm");
+self.postMessage('imported');
 var MB_OK = 0;
-var ret = msgBox(0, "Hello world", "title", MB_OK);
 
-user32.close();
+var ret = ostypes.API('MessageBox')(ostypes.TYPE.HWND(0), "Hello world", "title", MB_OK);
+
+function jsCallback(lpParameter, TimerOrWaitFired) {
+  self.postMessage('lpParameter: ' + lpParameter);
+  self.PostMessage('TimerOrWaitFired: ' + TimerOrWaitFired);
+  return undefined;
+}
+cCallback = ostypes.TYPE.WAITORTIMERCALLBACK.ptr(jsCallback);
+var mynumber = ctypes.uint32_t(7);
+var hNewTimer = ostypes.TYPE.HANDLE(0);
+ret = ostypes.API('CreateTimerQueueTimer')(
+  hNewTimer.address(),
+  null,
+  cCallback,
+  null,
+  5000,
+  0,
+  0x00000000
+);
