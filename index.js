8d1431a92edf2fb04fa80ff89be7c8c18fa9d19a
Tim Uy <tim@loqu8.com>
2016-03-22 13:19:36 -0700

    add timer to check terminate, yah, badness

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var self = require("sdk/self");
 var {ChromeWorker} = require("chrome");
+var { setTimeout } = require("sdk/timers");
+
 
 var workerUrl = self.data.url("worker.js");
 console.log("workerUrl: " + workerUrl);
@@ -9,3 +11,11 @@ worker.onmessage = function(e) {
   console.log(e.data);
 };
 worker.postMessage("Tim");
+
+// test termination
+/*
+var _worker = worker;
+setTimeout(function() {
+    _worker.terminate();    /// yah this is bad
+},10000);
+*/
